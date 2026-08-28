6896bb75147a18ed88ab27c9ddbff5e6dd8ed51f
Jonathan Ibie <xander@Jonathans-MacBook-Air.local>
2026-08-28 18:02:29 +0100

    Update vite config with chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Raises the warning limit to 1000 kB
+  },
   // server: {
   //   watch: {
   //     usePolling: true,
